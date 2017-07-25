25bf8184e0a2d20fb6753156a1e2076b9257df90
Emir <swifterfit@gmail.com>
2017-07-25 23:08:05 +0800

    fix webview landscape play video status bar bug

--- SSScreenRotation/SecondViewController.m
+++ SSScreenRotation/SecondViewController.m
@@ -21,6 +21,17 @@
 
     NSString *urlString = @"https://cn.bing.com/videos/search?q=%E5%AD%99%E6%9D%A8%E7%A2%BE%E5%8E%8B%E5%A4%8D%E4%BB%87%E9%9C%8D%E9%A1%BF&FORM=VSTREQ";
     [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlString]]];
+    
+#warning 暂时解决webview全屏播放视频status bar问题
+    [[NSNotificationCenter defaultCenter] addObserverForName:@"UIWindowDidRotateNotification" object:nil queue:nil usingBlock:^(NSNotification *note) {
+        if ([note.userInfo[@"UIWindowOldOrientationUserInfoKey"] intValue] >= 3) {
+            self.navigationController.navigationBar.frame = (CGRect){0, 0, self.view.frame.size.width, 64};
+        }
+    }];
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self forKeyPath:@"UIWindowDidRotateNotification"];
 }
 
 - (void)didReceiveMemoryWarning {
